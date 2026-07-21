20a7d03289c7b5d85b63eabfaf2ab9110f6fe734
David Scherfgen <d.scherfgen@googlemail.com>
2026-07-21 21:18:36 +0200

    Remove duplicate COND clause in SDIFF

--- src/comm.lisp
+++ src/comm.lisp
@@ -324,7 +324,6 @@
 	   (mul2 (factor (sdiff efact x)) (div e efact))))
 	((eq (caar e) '%integrate) (diffint e x))
 	((eq (caar e) '%laplace) (difflaplace e x))
-	((eq (caar e) '%at) (diff-%at e x))
 ; This rule is not correct. We cut it out.
 ;	((member (caar e) '(%realpart %imagpart) :test #'eq)
 ;	 (list (cons (caar e) nil) (sdiff (cadr e) x)))
